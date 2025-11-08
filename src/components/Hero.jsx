d8f140d184962d7487644c8150e379aa25eff8fa
codexmish <majharulislam.shanto.70@gmail.com>
2025-11-08 21:56:32 +0600

    some fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,13 +3,6 @@ import React from 'react'
 const Hero = () => {
   return (
     <>
-    <div className='w-full h-1000'>
-        <div className='w-full h-200 bg-green-400'></div>
-        <div className='w-full h-200 bg-blue-400'></div>
-        <div className='w-full h-200 bg-gray-400'></div>
-        <div className='w-full h-200 bg-red-400'></div>
-        <div className='w-full h-200 bg-purple-400'></div>
-    </div>
     
     </>
   )
