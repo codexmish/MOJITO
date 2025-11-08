8da25c43947eec19cf028a62bffb4ea5c837f126
codexmish <majharulislam.shanto.70@gmail.com>
2025-11-08 21:02:34 +0600

    setup done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <div>
+          <a href="#home" className="flex items-center gap-2"></a>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
